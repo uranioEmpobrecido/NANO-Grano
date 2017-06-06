33122ecc7a8ce7ee7c6cd1ce9f5364b19219c09a
vibiss <jorgegrolmos@gmail.com>
2017-06-06 21:09:26 +0200

    Added 13 Buttons
    Not tested

--- GranularSynth.ino
+++ GranularSynth.ino
@@ -1,5 +1,3 @@
-
-//
 // Analog in 0: Grain 1 pitch
 // Analog in 1: Octave Selector
 // Analog in 2: Grain 1 decay
@@ -17,67 +15,34 @@ uint16_t grainAmp;
 uint8_t grainDecay;
 
 // Map Analogue channels
-//#define SYNC_CONTROL         (4)
 #define GRAIN_FREQ_CONTROL   (0)
 #define GRAIN_DECAY_CONTROL  (2)
 #define OCTAVE_CONTROL       (1)
 
 // Map Digital channels
+#define C         PB5
+#define Csharp    PB4
+#define D         PB3
+#define Dsharp    PB2
+#define E         PB1
+#define F         PB0
+#define Fsharp    PD7
+#define G         PD6
+#define Gsharp    PD5
+#define A         PB7
+#define Asharp    PB6
+#define B         PD4
+#define Cupper    PD2
 
-/*
-#define C         13
-#define Csharp    12
-#define D         11
-#define Dsharp    10
-#define E         9
-#define F         8
-#define Fsharp    7
-#define G         6
-#define Gsharp    5
-#define A         4
-#define Asharp    3
-#define B         2
-#define Cupper    1
-*/
 
 // Changing these will also requires rewriting audioOn()
-
-#if defined(__AVR_ATmega8__)
-//
-// On old ATmega8 boards.
-//    Output is on pin 11
-//
-#define LED_PIN       2
-#define LED_PORT      PORTD
-#define LED_BIT       5
-#define PWM_PIN       11
-#define PWM_VALUE     OCR2
-#define PWM_INTERRUPT TIMER2_OVF_vect
-#elif defined(__AVR_ATmega1280__)
-//
-// On the Arduino Mega
-//    Output is on pin 3
-//
-//#define LED_PIN       2
-//#define LED_PORT      PORTD
-#define LED_PIN       13
-#define LED_PORT      PORTB
-//#define LED_BIT       7
-#define PWM_PIN       3
-#define PWM_VALUE     OCR3C
-#define PWM_INTERRUPT TIMER3_OVF_vect
-#else
-//
 // For modern ATmega168 and ATmega328 boards
-//    Output is on pin 3
-//
-#define PWM_PIN       3
+#define PWM_PIN       PC3
 #define PWM_VALUE     OCR2B
 #define LED_PIN       2
 #define LED_PORT      PORTD
 #define LED_BIT       5
 #define PWM_INTERRUPT TIMER2_OVF_vect
-#endif
 
 uint16_t freqMap, octaveMap;
 
@@ -94,22 +59,6 @@ uint16_t antilogTable[] = {
 };
 uint16_t mapPhaseInc(uint16_t input) {
   return (antilogTable[input & 0x3f]) >> (input >> 6);
-}
-
-// Stepped chromatic mapping
-//
-uint16_t midiTable[] = {
-  17,18,19,20,22,23,24,26,27,29,31,32,34,36,38,41,43,46,48,51,54,58,61,65,69,73,
-  77,82,86,92,97,103,109,115,122,129,137,145,154,163,173,183,194,206,218,231,
-  244,259,274,291,308,326,346,366,388,411,435,461,489,518,549,581,616,652,691,
-  732,776,822,871,923,978,1036,1097,1163,1232,1305,1383,1465,1552,1644,1742,
-  1845,1955,2071,2195,2325,2463,2610,2765,2930,3104,3288,3484,3691,3910,4143,
-  4389,4650,4927,5220,5530,5859,6207,6577,6968,7382,7821,8286,8785,9301,9854,
-  10440,11060,11718,12415,13153,13935,14764,15642,16572,17557,18601,19708,20885,
-  22121,23436,24830,26306
-};
-uint16_t mapMidi(uint16_t input) {
-  return (midiTable[(1023-input) >> 3]);
 }
 
 // Vibiss mapping OCTAVE 2
@@ -146,27 +95,48 @@ uint16_t Octave6Table[] = {
 
 uint16_t mapOctave() {
 
-  /*input = analogRead(SYNC_CONTROL);
-  // note = ((-0.00002*input*input)+(0.0435*input)-9.15);*/
   if (digitalRead(8)==0){
     selectOctave(0);
   }
-  else if (digitalRead(9)==0){
+  else if (digitalRead(C)==0){
+    selectOctave(0);
+  }
+  else if (digitalRead(Csharp)==0){
     selectOctave(1);
   }
-  else if (digitalRead(10)==0){
+  else if (digitalRead(D)==0){
     selectOctave(2);
   }
-  else if (digitalRead(11)==0){
+  else if (digitalRead(Dsharp)==0){
     selectOctave(3);
   }
-  else if (digitalRead(12)==0){
+  else if (digitalRead(E)==0){
     selectOctave(4);
   }
-  else if (digitalRead(13)==0){
+  else if (digitalRead(F)==0){
     selectOctave(5);
   }
-  
+  else if (digitalRead(Fsharp)==0){
+    selectOctave(6);
+  }
+  else if (digitalRead(G)==0){
+    selectOctave(7);
+  }
+  else if (digitalRead(Gsharp)==0){
+    selectOctave(8);
+  }
+  else if (digitalRead(A)==0){
+    selectOctave(9);
+  }
+  else if (digitalRead(Asharp)==0){
+    selectOctave(10);
+  }
+  else if (digitalRead(B)==0){
+    selectOctave(11);
+  }
+  else if (digitalRead(Cupper)==0){
+    selectOctave(12);
+  }
 }
 
 
@@ -206,52 +176,33 @@ uint16_t selectOctave(uint8_t note){
      Serial.print(Octave6Table[11-note]);
     return Octave6Table[11-note];
   }
+  
 }
 
 void audioOn() {
-#if defined(__AVR_ATmega8__)
-  // ATmega8 has different registers
-  TCCR2 = _BV(WGM20) | _BV(COM21) | _BV(CS20);
-  TIMSK = _BV(TOIE2);
-#elif defined(__AVR_ATmega1280__)
-  TCCR3A = _BV(COM3C1) | _BV(WGM30);
-  TCCR3B = _BV(CS30);
-  TIMSK3 = _BV(TOIE3);
-#else
   // Set up PWM to 31.25kHz, phase accurate
   TCCR2A = _BV(COM2B1) | _BV(WGM20);
   TCCR2B = _BV(CS20);
   TIMSK2 = _BV(TOIE2);
-#endif
 }
 
 void setup() {
   
   pinMode(PWM_PIN,OUTPUT);
   
-  pinMode(13,INPUT_PULLUP);
-  pinMode(12,INPUT_PULLUP);
-  pinMode(11,INPUT_PULLUP);
-  pinMode(10,INPUT_PULLUP);
-  pinMode(9,INPUT_PULLUP);
-  pinMode(8,INPUT_PULLUP);
-  
-  
-  /*
-  pinMode(C,INPUT);
-  pinMode(Csharp,INPUT);
-  pinMode(D,INPUT);
-  pinMode(Dsharp,INPUT);
-  pinMode(E,INPUT);
-  pinMode(F,INPUT);
-  pinMode(Fsharp,INPUT);
-  pinMode(G,INPUT);
-  pinMode(Gsharp,INPUT);
-  pinMode(A,INPUT);
-  pinMode(Asharp,INPUT);
-  pinMode(B,INPUT);
-  pinMode(Cupper,INPUT);
-  */
+  pinMode(C,INPUT_PULLUP);
+  pinMode(Csharp,INPUT_PULLUP);
+  pinMode(D,INPUT_PULLUP);
+  pinMode(Dsharp,INPUT_PULLUP);
+  pinMode(E,INPUT_PULLUP);
+  pinMode(F,INPUT_PULLUP);
+  pinMode(Fsharp,INPUT_PULLUP);
+  pinMode(G,INPUT_PULLUP);
+  pinMode(Gsharp,INPUT_PULLUP);
+  pinMode(A,INPUT_PULLUP);
+  pinMode(Asharp,INPUT_PULLUP);
+  pinMode(B,INPUT_PULLUP);
+  pinMode(Cupper,INPUT_PULLUP);
 /*
   DDRB = 0b00000000;    
   PORTB = 0b11111111;  
@@ -264,15 +215,8 @@ void setup() {
 }
 
 void loop() {
-  // The loop is pretty simple - it just updates the parameters for the oscillators.
-  //
-  // Avoid using any functions that make extensive use of interrupts, or turn interrupts off.
-  // They will cause clicks and poops in the audio.
   
-  // Smooth frequency mapping
-  //syncPhaseInc = mapPhaseInc(analogRead(SYNC_CONTROL)) / 4;
-  
-  // Stepped mapping to MIDI notes: C, Db, D, Eb, E, F...
+  // Stepped Fotomapping to MIDI notes: C, Db, D, Eb, E, F...
   syncPhaseInc = mapOctave();
   
   // Stepped pentatonic mapping: D, E, G, A, B
