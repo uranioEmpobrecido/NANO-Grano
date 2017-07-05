ad5eec0ffefcde8a947530da01952dc149bc6270
vibiss <jorgegrolmos@gmail.com>
2017-07-05 15:59:55 +0200

    Prototype Tested
    AD not implemented

--- GranularSynth/GranularSynth.ino
+++ GranularSynth/GranularSynth.ino
@@ -32,8 +32,8 @@ uint8_t grainDecay;
 #define Gsharp    5 //PD5
 #define A         4 //PD4
 #define Asharp    2 //PD2
-#define B         A4//PC4
-#define Cupper    A5//PC5
+#define B         1 //PD1
+#define Cupper    0 //PD0
 
 // Changing these will also requires rewriting audioOn()
 // For modern ATmega168 and ATmega328 boards
@@ -95,44 +95,44 @@ uint16_t Octave6Table[] = {
 
 uint16_t mapOctave() {
   if (digitalRead(C)==0){
-    return selectOctave(9);
+    return selectOctave(12);
   }
-  /*else if (digitalRead(Csharp)==0){
-    return selectOctave(1);
-  }*/
-  else if (digitalRead(D)==0){
-    return selectOctave(7);
-  }
- /* else if (digitalRead(Dsharp)==0){
-    return selectOctave(3);
-  }*/
-  else if (digitalRead(E)==0){
-    return selectOctave(5);
-  }
-  else if (digitalRead(F)==0){
-    return selectOctave(4);
-  }
- /* else if (digitalRead(Fsharp)==0){
-    return selectOctave(6);
-  }*/
-  else if (digitalRead(G)==0){
-    return selectOctave(2);
-  }
-  /*else if (digitalRead(Gsharp)==0){
-    return selectOctave(8);
-  }*/
-  else if (digitalRead(A)==0){
-    return selectOctave(0);
-  }
- /* else if (digitalRead(Asharp)==0){
-    return selectOctave(10);
-  }
-  else if (digitalRead(B)==0){
+  else if (digitalRead(Csharp)==0){
     return selectOctave(11);
   }
-  else if (digitalRead(Cupper)==0){
-    return selectOctave(12);
+  else if (digitalRead(D)==0){
+    return selectOctave(10);
+  }
+  else if (digitalRead(Dsharp)==0){
+    return selectOctave(9);
+  }
+  else if (digitalRead(E)==0){
+    return selectOctave(8);
+  }
+  else if (digitalRead(F)==0){
+    return selectOctave(7);
+  }
+  else if (digitalRead(Fsharp)==0){
+    return selectOctave(6);
+  }
+  else if (digitalRead(G)==0){
+    return selectOctave(5);
+  }
+  else if (digitalRead(Gsharp)==0){
+    return selectOctave(4);
+  }
+  else if (digitalRead(A)==0){
+    return selectOctave(3);
+  }
+  else if (digitalRead(Asharp)==0){
+    return selectOctave(2);
+  }
+  /*else if (digitalRead(B)==0){
+    return selectOctave(1);
   }*/
+  else if (digitalRead(Cupper)==0){
+    return selectOctave(0);
+  }
   else return 0;
 }
 
@@ -141,37 +141,37 @@ uint16_t selectOctave(uint8_t note){
 
   inputOct = analogRead(OCTAVE_CONTROL);
   octaveMap = (inputOct*5)/1024;
-  /*
+  
   Serial.print("  Note:");
   Serial.print(note);
   Serial.print("  Octave:");
   Serial.print(octaveMap);
   Serial.print("\n");
-  */
+  
   if (octaveMap == 0){
-    /* Serial.print("Freq:");
-     Serial.print(Octave2Table[11-note]);*/
-    return Octave2Table[11-note];
+     Serial.print("Freq:");
+     Serial.print(Octave2Table[11-note]);
+    return Octave2Table[12-note];
   }
   if (octaveMap == 1){
-    // Serial.print("Freq:");
-     //Serial.print(Octave3Table[11-note]);
-    return Octave3Table[11-note];
+     Serial.print("Freq:");
+     Serial.print(Octave3Table[11-note]);
+    return Octave3Table[12-note];
   }
   if (octaveMap== 2){
-     //  Serial.print("Freq:");
-    // Serial.print(Octave4Table[11-note]);
-    return Octave4Table[11-note];
+     Serial.print("Freq:");
+     Serial.print(Octave4Table[11-note]);
+    return Octave4Table[12-note];
   }
   if (octaveMap == 3){
-      // Serial.print("Freq:");
-    // Serial.print(Octave5Table[11-note]);
-    return Octave5Table[11-note];
+     Serial.print("Freq:");
+     Serial.print(Octave5Table[11-note]);
+    return Octave5Table[12-note];
   }
   if (octaveMap == 4){
-      // Serial.print("Freq:");
-     //Serial.print(Octave6Table[11-note]);
-    return Octave6Table[11-note];
+     Serial.print("Freq:");
+     Serial.print(Octave6Table[11-note]);
+    return Octave6Table[12-note];
   }
 
 }
@@ -187,14 +187,7 @@ void setup() {
 
   pinMode(PWM_PIN,OUTPUT);
   audioOn();
-
-  pinMode(13,INPUT_PULLUP);
-  pinMode(12,INPUT_PULLUP);
-  pinMode(11,INPUT_PULLUP);
-  pinMode(10,INPUT_PULLUP);
-  pinMode(9,INPUT_PULLUP);
-  pinMode(8,INPUT_PULLUP);
-  /*
+  
   pinMode(C,INPUT_PULLUP);
   pinMode(Csharp,INPUT_PULLUP);
   pinMode(D,INPUT_PULLUP);
@@ -208,11 +201,7 @@ void setup() {
   pinMode(Asharp,INPUT_PULLUP);
   pinMode(B,INPUT_PULLUP);
   pinMode(Cupper,INPUT_PULLUP);
-  */
-/*
-  DDRB = 0b00000000;
-  PORTB = 0b11111111;
-  */
+  
   Serial.begin(9600);
 }
 
